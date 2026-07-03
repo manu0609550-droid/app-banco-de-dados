70ff15f1036db5696ea570863c085dc211bcf8c2
EMANUELE TARGINO DO NASCIMENTO <20251120640034@ifrn.local>
2026-07-02 22:01:33 -0300

    add models

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Obra(models.Model):
+    TIPO_CHOICES = [
+        ("Filme", "Filme"),
+        ("Séries", "Série"),
+    ]
+    titulo = models.CharField(max_length=150)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    ano = models.IntegerField()
+    genero = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __stre__(self):
+        return self.titulo
+    
